4bfcc8b39d572a0ef5a391541871194110398fe2
SamuelSPatrocinio <samuel.soares.patrocinio@gmail.com>
2025-05-08 20:13:15 -0300

    adding code with motor speed control

---
+++ cubli/cubli/cubli.ino
@@ -0,0 +1,134 @@
+#include "ESP32.h"
+#include <Wire.h>
+#include <EEPROM.h>
+#include "BluetoothSerial.h"
+#include "esp_bt_device.h"
+#include "esp_bt_main.h"
+#include "esp_gap_bt_api.h"
+ 
+#include <FastLED.h>
+ 
+BluetoothSerial SerialBT;
+ 
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32-Cube"); // Bluetooth device name
+ 
+  EEPROM.begin(EEPROM_SIZE);
+ 
+  pinMode(BRAKE1, OUTPUT);
+  pinMode(BRAKE2, OUTPUT);
+  pinMode(BRAKE3, OUTPUT);
+ 
+  digitalWrite(BRAKE1, HIGH);
+  digitalWrite(BRAKE2, HIGH);
+  digitalWrite(BRAKE3, HIGH);
+  
+  pinMode(DIR1, OUTPUT);
+  pinMode(ENC1_1, INPUT);
+  pinMode(ENC1_2, INPUT);
+  attachInterrupt(ENC1_1, ENC1_READ, CHANGE);
+  attachInterrupt(ENC1_2, ENC1_READ, CHANGE);
+  ledcSetup(PWM1_CH, BASE_FREQ, TIMER_BIT);
+  ledcAttachPin(PWM1, PWM1_CH);
+  Motor1_control(0);
+  
+  pinMode(DIR2, OUTPUT);
+  pinMode(ENC2_1, INPUT);
+  pinMode(ENC2_2, INPUT);
+  attachInterrupt(ENC2_1, ENC2_READ, CHANGE);
+  attachInterrupt(ENC2_2, ENC2_READ, CHANGE);
+  ledcSetup(PWM2_CH, BASE_FREQ, TIMER_BIT);
+  ledcAttachPin(PWM2, PWM2_CH);
+  Motor2_control(0);
+  
+  pinMode(DIR3, OUTPUT);
+  pinMode(ENC3_1, INPUT);
+  pinMode(ENC3_2, INPUT);
+  attachInterrupt(ENC3_1, ENC3_READ, CHANGE);
+  attachInterrupt(ENC3_2, ENC3_READ, CHANGE);
+  ledcSetup(PWM3_CH, BASE_FREQ, TIMER_BIT);
+  ledcAttachPin(PWM3, PWM3_CH);
+  Motor3_control(0);
+ 
+  EEPROM.get(0, offsets);
+  if (offsets.ID == 96)
+    calibrated = true;
+ 
+  delay(200);
+  angle_setup();
+ 
+}
+ 
+void loop() {
+  currentT = millis();
+  if (currentT - previousT_1 >= loop_time) {
+    Tuning();
+    angle_calc();
+
+    //Serial.print("X: ");
+    //Serial.print(robot_angleX);
+    //Serial.print(" Y: ");
+    //Serial.println(robot_angleY);
+ 
+    motor1_speed = enc_count1;
+    enc_count1 = 0;
+    motor2_speed = enc_count2;
+    enc_count2 = 0;
+    motor3_speed = enc_count3;
+    enc_count3 = 0;
+    threeWay_to_XY(motor1_speed, motor2_speed, motor3_speed);
+    motors_speed_Z = motor1_speed + motor2_speed + motor3_speed;
+    
+    if (vertical_vertex && calibrated && !calibrating) {    
+      applyBrakes();
+      gyroX = GyX / 131.0;
+      gyroY = GyY / 131.0;
+      gyroZ = GyZ / 131.0;
+      gyroXfilt = alpha * gyroX + (1 - alpha) * gyroXfilt;
+      gyroYfilt = alpha * gyroY + (1 - alpha) * gyroYfilt;
+      
+      // int pwm_X = constrain(K1 * robot_angleX + K2 * gyroXfilt + K3 * speed_X + K4 * motors_speed_X, -255, 255);
+      // int pwm_Y = constrain(K1 * robot_angleY + K2 * gyroYfilt + K3 * speed_Y + K4 * motors_speed_Y, -255, 255);
+      // int pwm_Z = constrain(zK2 * gyroZ + zK3 * motors_speed_Z, -255, 255);
+ 
+      // motors_speed_X += speed_X / 5;
+      // motors_speed_Y += speed_Y / 5;
+      // XYZ_to_threeWay(pwm_X, pwm_Y, pwm_Z);
+
+      // Moto aresta:
+      int pwm_X = constrain(eK1 * robot_angleX + eK2 * gyroXfilt + eK3 * motor3_speed + eK4 * motors_speed_X, -255, 255);
+      
+      motors_speed_X += motor3_speed / 5;
+      Motor3_control(pwm_X);
+ 
+ 
+    } else if (vertical_edge && calibrated && !calibrating) {
+      digitalWrite(BRAKE3, HIGH);
+      gyroX = GyX / 131.0;
+      gyroXfilt = alpha * gyroX + (1 - alpha) * gyroXfilt;
+      
+      int pwm_X = constrain(eK1 * robot_angleX + eK2 * gyroXfilt + eK3 * motor3_speed + eK4 * motors_speed_X, -255, 255);
+      
+      motors_speed_X += motor3_speed / 5;
+      Motor3_control(pwm_X);
+ 
+      
+    } else {
+      XYZ_to_threeWay(0, 0, 0);
+      releaseBrakes();
+      motors_speed_X = 0;
+      motors_speed_Y = 0;
+    }
+    previousT_1 = currentT;
+  }
+  
+  if (currentT - previousT_2 >= 2000) {    
+    if (!calibrated && !calibrating) {
+      SerialBT.println("first you need to calibrate the balancing points...");
+      Serial.println("first you need to calibrate the balancing points (over bluetooth)...");
+    }
+    previousT_2 = currentT;
+  }  
+}
+ 
